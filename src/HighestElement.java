b349d013ff7c8e598b5113bb0f5d1771e618c762
msateesh900 <sateeshnaidu900@gmail.com>
2024-09-24 16:09:04 +0530

    updated

--- src/HighestElement.java
+++ src/HighestElement.java
@@ -9,5 +9,12 @@ public class HighestElement {
 		List<Integer> SampleArray = new ArrayList<>(Arrays.asList(21, 34, 56, 34, 56, 27, 16, 17, 91));
 		Optional<Integer> result = SampleArray.stream().sorted(Comparator.reverseOrder()).limit(4).skip(1).findFirst();
 		System.out.println("3rd highest element:" + result.get());
+		
+		int array[] = { 21, 34, 56, 34, 56, 27, 16, 17, 91 };
+		int size = array.length;
+		Arrays.sort(array);
+		System.out.println("sorted Array ::" + Arrays.toString(array));
+		int max = array[size - 3];
+		System.out.println("3rd highest ::" + max);
 	}
 }
--- src/Test.java
+++ src/Test.java
@@ -26,12 +26,7 @@ public class Test {
 	public static void main(String[] args) {
 
 		
-		int array[] = { 21, 34, 56, 34, 56, 27, 16, 17, 91 };
-		int size = array.length;
-		Arrays.sort(array);
-		System.out.println("sorted Array ::" + Arrays.toString(array));
-		int max = array[size - 3];
-		System.out.println("3rd highest ::" + max);
+		
 
 		// even numbers
 		Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).filter(i -> i % 2 == 0).skip(2)
