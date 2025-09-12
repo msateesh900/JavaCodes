4085e82edccdc8c884a87df37fa72a0d7bd8e8a9
msateesh900 <sateeshnaidu900@gmail.com>
2025-09-12 08:08:49 +0530

    updated code scenarios

--- src/Test.java
+++ src/Test.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 public class Test {
 	
+	// global logic coding question wrong answer which tried during interview
 	public static boolean arrayTest(int[] arr1, int[] arr2) {
 		boolean order=false, result=false;
 		for(int i=0;i<arr1.length;i++) {
@@ -26,22 +27,25 @@ public class Test {
 		return result;
 	}
 
+	//global logic coding question answer
 	private static boolean subArrayTest(int[] arr1, int[] arr2) {
-//		Arrays.sort(arr1);
-//		Arrays.sort(arr2);
-//		int i=0,j=0;
-//		int m=arr1.length,n=arr2.length;
-//		while(i<m&&j<n) {
-//			if(arr1[i]<arr2[j]) {
-//				i++;
-//			}else if(arr1[i]==arr2[j]) {
-//				i++;
-//				j++;
-//			}else {
-//				return false;
-//			}
-//		}
-//		return (j==n);
+		// using Two pointer approach
+		/*  Arrays.sort(arr1);
+			Arrays.sort(arr2);
+			int i=0,j=0;
+			int m=arr1.length,n=arr2.length;
+			while(i<m&&j<n) {
+				if(arr1[i]<arr2[j]) {
+					i++;
+				}else if(arr1[i]==arr2[j]) {
+					i++;
+					j++;
+				}else {
+					return false;
+				}
+			}
+			return (j==n);
+		*/
 		Set<Integer> hashSet = new HashSet<>();
 		for(int num:arr1) {
 			hashSet.add(num);
@@ -53,59 +57,64 @@ public class Test {
 		}
 		return true;
 	}
+	
+	// 12/09/2025
+    public static void segregateEvenOdd(int[] arr) {
+    
+    	int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            // Increment left until an odd number is found
+            while (arr[left] % 2 == 0 && left < right) {
+                left++;
+            }
+
+            // Decrement right until an even number is found
+            while (arr[right] % 2 != 0 && left < right) {
+                right--;
+            }
+
+            // If left < right, swap the elements
+            if (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
 
 
-	public static char findFirstNonRepeatedCharacter(String str) {
-		Map<Character, Integer> characterCountMap = new LinkedHashMap<>();
-
-//	         Populate the map with character counts
-		for (char ch : str.toCharArray()) {
-			characterCountMap.put(ch, characterCountMap.getOrDefault(ch, 0) + 1);
-		}
-
-		System.out.println("map with character counts" + characterCountMap);
-//	         Find the first non-repeated character
-		for (Map.Entry<Character, Integer> entry : characterCountMap.entrySet()) {
-			if (entry.getValue() == 1)
-				return entry.getKey();
-		}
-		return 0;
-
-	}
 
 	public static void main(String[] args) {
+
+        int[] arr = {12, 34, 45, 9, 8, 90, 3, 7, 10};
+        segregateEvenOdd(arr);
+
+        System.out.print("Array after segregation: ");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
 
 		int[] arr1 = {-1, 2, 7 ,8, 22, 34, 100};
 		int[] arr2 = {-1,2,100};
 //		subArrayTest(arr1,arr2);
-		System.out.print(subArrayTest(arr1,arr2));
+		System.out.print("subArrayTest:"+subArrayTest(arr1,arr2));
 		
 //		A----> B(load increases) 
-		int[] arr= {1,4,5,6,7,8,9};
+		int[] arr3= {1,4,5,6,7,8,9};
 		// third largest distinct odd number
-		Integer res= Arrays.stream(arr).boxed().map(i->i).sorted(Comparator.reverseOrder()).filter(i->i%2==1).skip(2).findFirst().get();
-		System.out.println(res);
+		Integer res= Arrays.stream(arr3).boxed().map(i->i).sorted(Comparator.reverseOrder()).filter(i->i%2==1).skip(2).findFirst().get();
+		System.out.println("third largest distinct odd number:"+res);
 
 		// even numbers
 		Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).filter(i -> i % 2 == 0).skip(2)
 				.forEach(i -> System.out.print(i + " "));
 
-		List<String> g = Arrays.asList("geeks", "for", "geeks");
 
-//     Collect the list as map
-//     by groupingBy() method
-		Map<String, Long> result2 = g.stream()
-				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-//     Print the result
-		System.out.println(result2);
-
-		String input = "swiss";
-		char result1 = findFirstNonRepeatedCharacter(input);
-		if (result1 != '\0') {
-			System.out.println("The first non-repeated character is: " + result1);
-		} else {
-			System.out.println("No non-repeated character found.");
-		}
 	}
 }
