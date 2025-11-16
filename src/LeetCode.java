c276a41ca2ef1c80574b88b06165a81619585159
sateesh <sateeshnaidu900@gmail.com>
2025-11-16 08:11:50 +0530

    removed Solutions class

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -35,22 +35,22 @@ public class LeetCode {
 		/* Two Sum examples*/
 		int[] arr1 = {2, 7, 11, 15};
         int target1 = 9;
-        int[] result1 = Solution.twoSum(arr1, target1);
+        int[] result1 = twoSum(arr1, target1);
         System.out.println("Array: " + Arrays.toString(arr1) + ", Target: " + target1 + ", Result: " + Arrays.toString(result1)); // Output: [2, 7]
 
         int[] arr2 = {3, 2, 4};
         int target2 = 6;
-        int[] result2 = Solution.twoSum(arr2, target2);
+        int[] result2 = twoSum(arr2, target2);
         System.out.println("Array: " + Arrays.toString(arr2) + ", Target: " + target2 + ", Result: " + Arrays.toString(result2)); // Output: [2, 4] (after sorting)
 
         int[] arr3 = {1, 2, 3, 4, 5};
         int target3 = 10;
-        int[] result3 = Solution.twoSum(arr3, target3);
+        int[] result3 = twoSum(arr3, target3);
         System.out.println("Array: " + Arrays.toString(arr3) + ", Target: " + target3 + ", Result: " + Arrays.toString(result3)); // Output: []
 
         int[] arr4 = { 3,3 };
 		int target4 = 6;
-        int[] result4 = Solution.twoSum(arr4, target4);        
+        int[] result4 = twoSum(arr4, target4);        
         System.out.println("Array: " + Arrays.toString(arr4) + ", Target: " + target4 + ", Result: " + Arrays.toString(result4)); // Output: [2, 7]
         /* Two Sum examples end*/
         
