b4efa6736eda0ebdb517304cc16597fea8bda2da
sateesh <sateeshnaidu900@gmail.com>
2025-11-16 08:30:48 +0530

    added Number reverse program

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -12,6 +12,17 @@ public class LeetCode {
 			return new int[] {};
 		}
 	
+	public static int NumReverse(int num) {
+		long revNum = 0;
+		while (num != 0) {
+			revNum = revNum * 10 + num % 10;
+			num = num / 10;
+			if (revNum > Integer.MAX_VALUE || revNum < Integer.MIN_VALUE)
+				return 0;
+		}
+		return (int) revNum;
+	}
+	
 	public static int search(int[] nums, int target) {
 		int left = 0, right = nums.length - 1;
 		int mid = 0;
@@ -55,5 +66,16 @@ public class LeetCode {
         /* Two Sum examples end*/
         
 
+		/* Num reverse examples */
+		int num1 = 121;
+		System.out.println("Number reverse Ex1 result:" + NumReverse(num1));
+		int num2 = -123;
+		System.out.println("Number reverse Ex2 result:" + NumReverse(num2));
+		int num3 = 120;
+		System.out.println("Number reverse Ex3 result:" + NumReverse(num3));
+		int num4=1534236469;
+		System.out.println("Number reverse Ex4 result:" + NumReverse(num4));
+		/* Num reverse examples end */
+		
 	}
 }
